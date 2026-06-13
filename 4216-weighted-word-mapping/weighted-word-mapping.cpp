02c5ab5bd392690eac5ca528412ebaa222a2dc29
Krish Sharma <godofwarghostofsparta159@gmail.com>
2026-06-13 10:16:48 +0530

    Time: 3 ms (27.99%) | Memory: 43.6 MB (77.60%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+
+        for (const string& word : words) {
+            int sum = 0;
+
+            for (char ch : word) {
+                sum += weights[ch - 'a'];
+            }
+
+            int rem = sum % 26;
+
+            ans.push_back('z' - rem);
+        }
+
+        return ans;
+    }
+};
