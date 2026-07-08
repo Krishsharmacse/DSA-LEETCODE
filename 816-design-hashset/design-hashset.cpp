6a6832c09dab27f7b47e3c4d9275fe9e685bdb25
Krish Sharma <godofwarghostofsparta159@gmail.com>
2026-07-08 09:19:51 +0530

    Time: 15 ms (56.53%) | Memory: 48.1 MB (79.97%) - LeetSync

--- 816-design-hashset/design-hashset.cpp
+++ 816-design-hashset/design-hashset.cpp
@@ -1,18 +1,31 @@
 #include <unordered_set>
-
 class MyHashSet {
-    unordered_set<int> s;
-
 public:
+  
+        unordered_set<int>map;
+        
+    
+    
     void add(int key) {
-        s.insert(key);
+        map.insert(key);
+        
     }
-
+    
     void remove(int key) {
-        s.erase(key);
+        map.erase(key);
+        
     }
-
+    
     bool contains(int key) {
-        return s.find(key) != s.end();
+        return map.find(key)!=map.end();
+        
     }
 };
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
